35cd103f7693da105a3c3063bd6ec54489f6abec
Chris Ashworth <chris@figure53.com>
2015-09-16 10:12:19 -0400

    Fix how pager maintains "approved" state.

--- site/admin/_pager.php
+++ site/admin/_pager.php
@@ -1,11 +1,11 @@
 <a href="?page=<?php 
     echo $prev_page;
-    if (!is_null($message_approved))
-        echo "&approved=$message_approved";?>">&larr; Previous</a>
+    if (!is_null($message_show_approved))
+        echo "&approved=$message_show_approved";?>">&larr; Previous</a>
 
 | Page <?php echo $page ?> of <?php echo $total_pages ?> | 
 
 <a href="?page=<?php 
     echo $next_page;
-    if (!is_null($message_approved))
-        echo "&approved=$message_approved";?>">Next &rarr;</a>
+    if (!is_null($message_show_approved))
+        echo "&approved=$message_show_approved";?>">Next &rarr;</a>
